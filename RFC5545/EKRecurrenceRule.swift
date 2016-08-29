17b743210f5179b64d4ce690ce4ee305a1cdcb56
Scott Grosch <Scott.Grosch@icloud.com>
2016-08-29 00:08:31 -0700

    Added a TODO for the UNTIL RRULE.

--- RFC5545/EKRecurrenceRule.swift
+++ RFC5545/EKRecurrenceRule.swift
@@ -56,7 +56,8 @@ extension EKRecurrenceRule {
      *
      *  - Returns: The generated RFC5545 RRULE string.
      *
-     *  - SeeAlso: [RFC5545 RRULE](https://tools.ietf.org/html/rfc5545#section-3.8.5.3)
+     * - SeeAlso: [RFC5545 RRULE](https://tools.ietf.org/html/rfc5545#section-3.8.5.3)
+     * - SeeAlso: [RFC5545 RECUR](https://tools.ietf.org/html/rfc5545#section-3.3.10)
      */
     func rfc5545() -> String {
         let freq: String
@@ -84,9 +85,26 @@ extension EKRecurrenceRule {
 
         if let end = recurrenceEnd {
             if let date = end.endDate {
-                // TODO: According the the RFC doc, "should be specified as a date with local
-                // time and time zone"...so this should change from .utc to something else.  That
-                // means we are going to need a new enum value so specify a local time
+                /*
+                 TODO:  Implement this timezone stuff from the RFC spec:
+                 
+                 The UNTIL rule part defines a DATE or DATE-TIME value that bounds
+                 the recurrence rule in an inclusive manner.  If the value
+                 specified by UNTIL is synchronized with the specified recurrence,
+                 this DATE or DATE-TIME becomes the last instance of the
+                 recurrence.  The value of the UNTIL rule part MUST have the same
+                 value type as the "DTSTART" property.  Furthermore, if the
+                 "DTSTART" property is specified as a date with local time, then
+                 the UNTIL rule part MUST also be specified as a date with local
+                 time.  If the "DTSTART" property is specified as a date with UTC
+                 time or a date with local time and time zone reference, then the
+                 UNTIL rule part MUST be specified as a date with UTC time.  In the
+                 case of the "STANDARD" and "DAYLIGHT" sub-components the UNTIL
+                 rule part MUST always be specified as a date with UTC time.  If
+                 specified as a DATE-TIME value, then it MUST be specified in a UTC
+                 time format.  If not present, and the COUNT rule part is also not
+                 present, the "RRULE" is considered to repeat forever.
+                 */
                 text += ";UNTIL=\(date.rfc5545(format: .utc))"
             } else {
                 text += ";COUNT=\(end.occurrenceCount)"
